0c442975a7a3ab8726354a939b3dff7628911e82
Florencia Tabarez <fftabarez97@gmail.com>
2020-06-02 08:45:37 -0600

    Backend complete

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Brian's Burger", true), ("Sam's Burger", true);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Flofu Burger", true);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Gross Burger", false);
+
