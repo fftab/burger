5d53f17a5e69df6c71ee9c4a38ec685dc33eda8a
Florencia Tabarez <fftabarez97@gmail.com>
2020-06-23 22:02:50 -0600

    Preparing for Heroku Launch.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,30 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  // allows access to connection by port num
-  port: 3306,
-  user: "root",
-  password: "5632MgH!",
-  database: "burgers_db"
-});
+// Local Connection =====================
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   // allows access to connection by port num
+//   port: 3306,
+//   user: "root",
+//   password: "5632MgH!",
+//   database: "burgers_db"
+// });
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '5632MgH!',
+        database: 'burgers_db'
+    })
+};
 
 // Make connection
 connection.connect(function(err) {
